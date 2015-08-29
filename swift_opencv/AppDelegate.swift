ac68970bdb5cf522aebbf53d408b67f0dacc25b6
Eric Willenson <e.willenson@bigspaceship.com>
2015-08-29 14:53:22 -0400

    Initial Commit

---
+++ swift_opencv/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  swift_opencv
+//
+//  Created by Eric Willenson on 8/29/15.
+//  Copyright (c) 2015 Eric Willenson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ swift_opencv/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swift_opencv
+//
+//  Created by Eric Willenson on 8/29/15.
+//  Copyright (c) 2015 Eric Willenson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
